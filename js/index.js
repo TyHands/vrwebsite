9dcccc5d616dc38a53ae648b67d2410f917ebb1b
Tyran Hands <tyran.hands@vintagerevelations.co.uk>
2026-04-02 14:10:19 +0100

    Changed landing page intial picture and navbar in js

--- js/index.js
+++ js/index.js
@@ -14,22 +14,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
   let navbar = document.getElementById("vrnav")
   navbar.innerHTML = 
-  '<div class="row valign-wrapper white lato-custom no-margin">' +
-  '<div class="custom-nav col s6 nav-wrapper">' +
-      '<a href="index.html"><img src="res/VRLOGO.png" class="left logo_img" href="index.html"></a>' +
+'<div class="row valign-wrapper white lato-nav no-margin nav-shadow nav-sizing">' +
+  '<div class="custom-nav col s1 nav-wrapper">' +
+    '<a href="index.html"><img src="res/VRLOGO.png" class="left logo_img" href="index.html"></a>' +
   '</div>' +
   '<div class="custom-nav col s6 nav-wrapper">' +
     '<a href="#" data-target="dropdown" class="sidenav-trigger black-text right menu-bar"><i class="small material-icons">menu</i></a>' +
   '</div>' +
-  '<div class="col s8 hide-on-med-and-down">' +
-      '<a href="careers.html" class="black-text custom-nav-hover nav-button col s3 center-align">Careers</a>' +
-      '<a href="about.html" class="black-text custom-nav-hover col s3 center-align">About</a>' +
-      '<a href="services.html" class="black-text custom-nav-hover col s3 center-align">Services</a>' +
-      '<a href="contact.html" class="black-text custom-nav-hover col s3 center-align">Contact</a>' +
+  '<div class="col s11 hide-on-med-and-down">' +
+    '<a href="careers.html" class="black-text custom-nav-hover nav-button col s2 center-align">Careers</a>' +
+    '<a href="about.html" class="black-text custom-nav-hover nav-button col s2 center-align">About</a>' +
+    '<a href="services.html" class="black-text custom-nav-hover nav-button col s2 center-align">Services</a>' +
+    '<a href="bloglist.html" class="black-text custom-nav-hover nav-button col s2 center-align">Stories</a>' +
+    '<a href="contact.html" class="white-text center-align nav-button vr-gold">Enquire Now</a>' +
   '</div>' +
-  '<div class="col s1"></div>' +
 '</div>' +
-'<div class="banner-attachment center-align">Join the Everyday Heroes – <a href="https://vintagerevelations.co.uk/careers" class="black-text" target="_blank"><u>Apply now to become a carer today.</u></a></div>'
+'<div class="banner-container lato-custom hide-on-med-and-down">' +
+  '<div class="banner-left vr-gold">' +
+    '<a href="https://vintagerevelations.co.uk/carecalc" class="white-text" target="_blank">' +
+      'Use our free Care Calculator.' +
+    '</a>' +
+  '</div>' +
+  '<div class="banner-right antique-white">' +
+    '<a href="tel:01452947520" class="custom-gold-text lato-bold banner-phone">' +
+      '<i class="small material-icons">local_phone</i>' +
+      '01452 947520' +
+    '</a>' +
+  '</div>' +
+'</div>';
 
 
 function sendSubEmail(id){
